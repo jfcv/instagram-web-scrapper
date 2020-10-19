e24436b24926af9ce69b80a2da06d8142c1c3a71
velveet <velveet@pm.me>
2020-10-18 21:43:55 -0500

    business logic for 'mail.ts' api route implemented.

--- lib/transporter.ts
+++ lib/transporter.ts
@@ -3,15 +3,15 @@ import nodemailer from 'nodemailer'
 export const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD
+        user: process.env.NEXT_PUBLIC_EMAIL,
+        pass: process.env.NEXT_PUBLIC_PASSWORD
     }
 });
 
 export const mailOptions = (account: string, name: string, statsArr: Array<number>) => {
     const mailOptions = {
-        from: process.env.EMAIL,
-        to: process.env.EMAIL,
+        from: process.env.NEXT_PUBLIC_EMAIL,
+        to: process.env.NEXT_PUBLIC_EMAIL,
         subject: 'Scrapprtup Daily Report!',
         text: 'Dear customer, we are currently tracking the account: ' + name + '. It has ' + statsArr[0] + ' publications, ' + statsArr[1] + ' account followers '
          + ' & ' + statsArr[2] + ' accounts followed.' + ' If you want to access to this account click on the following link: ' + account + ' Have a nice day Scrapprter!'
--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -1,5 +1,39 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import {transporter, mailOptions} from '../../lib/transporter';
+import puppeteer from 'puppeteer'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+      /* request parameters */
+      const {account} = req.body;
 
+      /* puppeteer code */
+      const browser = await puppeteer.launch({headless: true});
+      const page = await browser.newPage();
+      await page.goto(account);
+      await page.waitForSelector('.zwlfE');
+      /* account name */
+      const name = await page.$eval('.zwlfE div.nZSzR h2', (h2: any) => h2.innerText);
+      /* account stats -> [ publications | followers | followed ] */
+      let statsArr = [];
+      const lis = await page.$$('ul.k9GMp li');
+      for (const li of lis) {
+          const result = await li.$eval('li.Y8-fY a.-nal3 span.g47SY', (span: any) => span.innerText);
+          statsArr.push(result);
+      }
+      await browser.close();
+      
+      const mailOptionsObj = mailOptions(account, name, statsArr);
+      const info = await transporter.sendMail(mailOptionsObj);
+      return res.json({
+          message: 'Mail sent successfully',
+          body: info
+      });
+  } catch (err) {
+      console.error(err);
+      return res.json({
+          message: 'Mail service not working !',
+          body: err
+      });  
+  }
 }
