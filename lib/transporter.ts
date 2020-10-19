dd9c47021bd7b712a6fe4800cd1a6602acefdc18
velveet <velveet@pm.me>
2020-10-18 21:27:18 -0500

    nodemailer installed. nodemailer types installed. 'transporter.ts' library added.

---
+++ lib/transporter.ts
@@ -0,0 +1,20 @@
+import nodemailer from 'nodemailer'
+
+export const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+});
+
+export const mailOptions = (account: string, name: string, statsArr: Array<number>) => {
+    const mailOptions = {
+        from: process.env.EMAIL,
+        to: process.env.EMAIL,
+        subject: 'Scrapprtup Daily Report!',
+        text: 'Dear customer, we are currently tracking the account: ' + name + '. It has ' + statsArr[0] + ' publications, ' + statsArr[1] + ' account followers '
+         + ' & ' + statsArr[2] + ' accounts followed.' + ' If you want to access to this account click on the following link: ' + account + ' Have a nice day Scrapprter!'
+    }
+    return mailOptions
+}
